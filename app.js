3f07d2e2009f72f9999c5d3a3187ed4b91b2ecd3
Rue Williams <19MEDIAFOOD81@GMAIL.COM>
2021-03-24 13:48:49 -0700

    name insert and pronoun selector

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const pageButton = document.getElementById('change-button');
 const pageInput = document.getElementById('new-name');
 const tagName = document.getElementById('tag-name');
+const tagPronouns = document.getElementById('tag-pronouns')
+const pronounSelector = document.getElementById('pronoun-list')
 
 pageButton.addEventListener('click', () => {
     tagName.textContent = pageInput.value;
+})
+
+pronounSelector.addEventListener('change', () => {
+    tagPronouns.textContent = pronounSelector.value;
 })
